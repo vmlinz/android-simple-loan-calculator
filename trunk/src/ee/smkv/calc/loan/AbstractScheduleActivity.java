74ae09a227996ec900b05520c9ea0bef7f18ac05
andrei.samkov <andrei.samkov@ad215699-5d60-9b94-db70-8f590cbb3547>
2011-09-01 15:03:34 +0000

    Support large screens

--- trunk/src/ee/smkv/calc/loan/AbstractScheduleActivity.java
+++ trunk/src/ee/smkv/calc/loan/AbstractScheduleActivity.java
@@ -17,79 +17,93 @@ import ee.smkv.calc.loan.utils.OkDialogWrapper;
 import java.io.File;
 
 
-public abstract class AbstractScheduleActivity extends Activity{
-    protected static final String UTF = "UTF-8";
-    WebView webview = null;
+public abstract class AbstractScheduleActivity extends Activity {
+  protected static final String UTF = "UTF-8";
+  WebView webview = null;
+  private AsyncTask generatingTask;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        webview = new WebView(getActivity());
-        WebSettings webSettings = webview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setSupportZoom(false);
-        webview.addJavascriptInterface(getActivity(), "schedule");
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    webview = new WebView(getActivity());
+    WebSettings webSettings = webview.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+    webSettings.setSupportZoom(false);
+    webview.addJavascriptInterface(getActivity(), "schedule");
 
-        setContentView(webview);
-        AsyncTask task = new AsyncTask() {
-            private final ProgressDialog dialog = new ProgressDialog(getActivity());
+    setContentView(webview);
+    generatingTask = new AsyncTask() {
+      private ProgressDialog dialog = null;
 
-            @Override
-            protected void onPreExecute() {
-                this.dialog.setMessage(getResources().getString(R.string.scheduleLoading));
-                this.dialog.show();
-            }
+      @Override
+      protected void onPreExecute() {
+        this.dialog = new ProgressDialog(getActivity());
+        this.dialog.setMessage(getResources().getString(R.string.scheduleLoading));
+        this.dialog.show();
+      }
 
-            @Override
-            protected Object doInBackground(Object... objects) {
-                final Loan loan = getLoan();
-                webview.loadDataWithBaseURL(null, createHtml(loan), "text/html", UTF, "about:blank");
-                return null;
-            }
+      @Override
+      protected Object doInBackground(Object... objects) {
+        final Loan loan = getLoan();
+        webview.loadDataWithBaseURL(null, createHtml(loan), "text/html", UTF, "about:blank");
+        return null;
+      }
 
-            @Override
-            protected void onPostExecute(final Object unused) {
-                if (this.dialog.isShowing()) {
-                    this.dialog.dismiss();
-                }
-            }
-
-        };
-        task.execute();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.schedulemenu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        try {
-            switch (item.getItemId()) {
-                case R.id.exportEmailMenu:
-                    Exporter.sendToEmail(getLoan(), getResources(), getActivity());
-                    break;
-                case R.id.exportExcelMenu:
-                    File file = Exporter.exportToCSVFile(getLoan(), getResources());
-                    new OkDialogWrapper(this, getResources().getString(R.string.fileCreated) + ' ' + file.getName()).show();
-                    break;
-
-            }
-        } catch (FileCreationException e) {
-            new ErrorDialogWrapper(this, e.getMessage()).show();
+      @Override
+      protected void onPostExecute(final Object unused) {
+        if (this.dialog != null && this.dialog.isShowing()) {
+          this.dialog.dismiss();
+          this.dialog = null;
         }
-        return super.onOptionsItemSelected(item);
+      }
+
+    };
+    generatingTask.execute();
+  }
+
+  @Override
+  protected void onStop() {
+    if (generatingTask != null){
+      generatingTask.cancel(true);
     }
+    super.onStop();
+  }
 
-    protected String getStyle() {
-        return "body{background:#FFF;color:#111; font-family:Verdana;}table{border-spacing:0px 0px; font-size:11px; width:100%}td{padding:5px;white-space:nowrap; }th{padding:5px;text-align:left;white-space:nowrap;}.odd{background:#EEE;}.odd th{border-bottom: 1px solid black;} .odd td{border-bottom: 1px solid #CCC;}.even{background:#FAFAFA;}#closeBtn{width:100%;padding:10px} table{border-radius: 5px;}";
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.schedulemenu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    try {
+      switch (item.getItemId()) {
+        case R.id.exportEmailMenu:
+          Exporter.sendToEmail(getLoan(), getResources(), getActivity());
+          break;
+        case R.id.exportExcelMenu:
+          File file = Exporter.exportToCSVFile(getLoan(), getResources());
+          new OkDialogWrapper(this, getResources().getString(R.string.fileCreated) + ' ' + file.getName()).show();
+          break;
+
+      }
     }
+    catch (FileCreationException e) {
+      new ErrorDialogWrapper(this, e.getMessage()).show();
+    }
+    return super.onOptionsItemSelected(item);
+  }
 
-    protected abstract Activity getActivity();
+  protected String getStyle() {
+    return "body{background:#FFF;color:#111; font-family:Verdana;}table{border-spacing:0px 0px; font-size:11px; width:100%}td{padding:5px;white-space:nowrap; }th{padding:5px;text-align:left;white-space:nowrap;}.odd{background:#EEE;}.odd th{border-bottom: 1px solid black;} .odd td{border-bottom: 1px solid #CCC;}.even{background:#FAFAFA;}#closeBtn{width:100%;padding:10px} table{border-radius: 5px;}";
+  }
 
-    protected abstract String createHtml(Loan loan);
+  protected abstract Activity getActivity();
 
-    protected abstract Loan getLoan();
+  protected abstract String createHtml(Loan loan);
+
+  protected abstract Loan getLoan();
 }

f9c2af47494a423de8b4b0b3eb1e39b58555735e
andrei.samkov@gmail.com <andrei.samkov@gmail.com@ad215699-5d60-9b94-db70-8f590cbb3547>
2013-04-19 13:46:04 +0000

    Bar Chart and removed schedule loading dialog.

--- branches/next/src/ee/smkv/calc/loan/ChartFragment.java
+++ branches/next/src/ee/smkv/calc/loan/ChartFragment.java
@@ -14,10 +14,13 @@ import ee.smkv.calc.loan.model.Loan;
 import ee.smkv.calc.loan.model.Payment;
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
+import org.achartengine.chart.BarChart;
 import org.achartengine.chart.LineChart;
 import org.achartengine.chart.PieChart;
+import org.achartengine.chart.RangeBarChart;
 import org.achartengine.model.CategorySeries;
 import org.achartengine.model.MultipleCategorySeries;
+import org.achartengine.model.RangeCategorySeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.*;
@@ -26,169 +29,152 @@ import java.util.List;
 
 public class ChartFragment extends SherlockFragment {
 
-    PieChart pieChart;
-    LineChart lineChart;
+  PieChart pieChart;
+  LineChart lineChart;
+  RangeBarChart barChart;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.chart, container, false);
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.chart, container, false);
 
-        Loan loan = StartActivity.loan;
+    Loan loan = StartActivity.loan;
 
-        addPieChart(view, loan);
-        addLineChart(view, loan);
+    addPieChart(view, loan);
+    addBarChart(view, loan);
+
+    return view;
+  }
 
 
 
-        return view;
+
+  private void addBarChart(View view, Loan loan) {
+    XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+    XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+
+    SimpleSeriesRenderer principalRenderer = new SimpleSeriesRenderer();
+    SimpleSeriesRenderer interestRenderer = new SimpleSeriesRenderer();
+    SimpleSeriesRenderer commissionRenderer = new SimpleSeriesRenderer();
+
+
+    principalRenderer.setColor(getResources().getColor(R.color.principal));
+    interestRenderer.setColor(getResources().getColor(R.color.interest));
+    commissionRenderer.setColor(getResources().getColor(R.color.commission));
+
+
+    //renderer.setOrientation(XYMultipleSeriesRenderer.Orientation.VERTICAL);
+    renderer.setAntialiasing(true);
+    renderer.setZoomEnabled(false, false);
+    renderer.setPanEnabled(false, false);
+    renderer.setExternalZoomEnabled(false);
+    renderer.setShowGrid(true);
+    renderer.setGridColor(getResources().getColor(R.color.Separator_Color));
+    renderer.setBackgroundColor(getResources().getColor(R.color.abs__background_holo_light));
+    renderer.setMarginsColor(getResources().getColor(R.color.abs__background_holo_light));
+    renderer.setAxisTitleTextSize(getResources().getDimension(R.dimen.abs__action_bar_subtitle_text_size));
+    renderer.setInScroll(false);
+    renderer.setBarSpacing(0.5);
+
+    renderer.setLabelsColor(Color.BLACK);
+
+
+    renderer.addSeriesRenderer(0, principalRenderer);
+    dataset.addSeries(0, new PrincipalSeries(getString(R.string.paymentPrincipal), loan.getPayments()).toXYSeries());
+
+    renderer.addSeriesRenderer(1, interestRenderer);
+    dataset.addSeries(1, new InterestSeries(getString(R.string.paymentInterest), loan.getPayments()).toXYSeries());
+
+    if (loan.hasAnyCommission()) {
+      renderer.addSeriesRenderer(2, commissionRenderer);
+      dataset.addSeries(2, new CommissionSeries(getString(R.string.paymentCommission), loan.getPayments()).toXYSeries());
+    }
+
+    barChart = new RangeBarChart(dataset, renderer, BarChart.Type.STACKED);
+    GraphicalView lineChartView = new GraphicalView(view.getContext(), barChart);
+
+    LinearLayout linePlaceholder = (LinearLayout)view.findViewById(R.id.lineChartPlaceholder);
+    linePlaceholder.addView(lineChartView);
+  }
+
+  private void addPieChart(View view, Loan loan) {
+    DefaultRenderer renderer = new DefaultRenderer();
+    renderer.setAntialiasing(true);
+    renderer.setExternalZoomEnabled(false);
+    renderer.setPanEnabled(false);
+    renderer.setZoomEnabled(false);
+
+    renderer.setLabelsColor(getResources().getColor(R.color.abs__bright_foreground_holo_light));
+    renderer.setLabelsTextSize(getResources().getDimension(R.dimen.abs__action_bar_subtitle_text_size));
+    renderer.setBackgroundColor(getResources().getColor(R.color.abs__background_holo_light));
+    renderer.setMargins(new int[]{10, 10, 10, 10});
+    CategorySeries series = new CategorySeries("Pie");
+
+    series.add(getString(R.string.paymentPrincipal), loan.getAmount().doubleValue());
+    SimpleSeriesRenderer principalRenderer = new SimpleSeriesRenderer();
+    principalRenderer.setDisplayChartValues(true);
+    principalRenderer.setColor(getResources().getColor(R.color.principal));
+    renderer.addSeriesRenderer(0, principalRenderer);
+
+
+    series.add(getString(R.string.paymentInterest), loan.getTotalInterests().doubleValue());
+    SimpleSeriesRenderer interestRenderer = new SimpleSeriesRenderer();
+    interestRenderer.setDisplayChartValues(true);
+    interestRenderer.setColor(getResources().getColor(R.color.interest));
+    renderer.addSeriesRenderer(1, interestRenderer);
+
+
+    if (loan.hasAnyCommission()) {
+      series.add(getString(R.string.paymentCommission), loan.getCommissionsTotal().doubleValue());
+      SimpleSeriesRenderer commissionRenderer = new SimpleSeriesRenderer();
+      commissionRenderer.setDisplayChartValues(true);
+      commissionRenderer.setColor(getResources().getColor(R.color.commission));
+      renderer.addSeriesRenderer(2, commissionRenderer);
     }
 
 
-
-    private void addLineChart(View view, Loan loan) {
-        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-
-        XYSeriesRenderer principalRenderer = new XYSeriesRenderer();
-        XYSeriesRenderer interestRenderer = new XYSeriesRenderer();
-        XYSeriesRenderer commissionRenderer = new XYSeriesRenderer();
-        XYSeriesRenderer paymentRenderer = new XYSeriesRenderer();
-
-        paymentRenderer.setColor(getResources().getColor(R.color.payment));
-        paymentRenderer.setLineWidth(2);
-        paymentRenderer.setFillBelowLine(true);
-        paymentRenderer.setFillBelowLineColor(getResources().getColor(R.color.paymentFill));
-
-        principalRenderer.setColor(getResources().getColor(R.color.principal));
-        principalRenderer.setLineWidth(2);
-        principalRenderer.setFillBelowLine(true);
-        principalRenderer.setFillBelowLineColor(getResources().getColor(R.color.principalFill));
-
-        interestRenderer.setColor(getResources().getColor(R.color.interest));
-        interestRenderer.setLineWidth(2);
-        interestRenderer.setFillBelowLine(true);
-        interestRenderer.setFillBelowLineColor(getResources().getColor(R.color.interestFill));
-
-        commissionRenderer.setColor(getResources().getColor(R.color.commission));
-        commissionRenderer.setLineWidth(2);
-        commissionRenderer.setFillBelowLine(true);
-        commissionRenderer.setFillBelowLineColor(getResources().getColor(R.color.commissionFill));
+    pieChart = new PieChart(series, renderer) {
+      @Override
+      public void draw(Canvas canvas, int x, int y, int width, int height, Paint paint) {
+        setCenterX(width / 2);
+        setCenterY(height / 2);
+        super.draw(canvas, x, y, width, height, paint);
+      }
+    };
+    GraphicalView pieChartView = new GraphicalView(view.getContext(), pieChart);
+    LinearLayout placeholder = (LinearLayout)view.findViewById(R.id.pieChartPlaceholder);
+    placeholder.addView(pieChartView);
+  }
 
 
-        renderer.setAntialiasing(true);
-        renderer.setZoomEnabled(false , false);
-        renderer.setPanEnabled(false , false);
-        renderer.setExternalZoomEnabled(false);
-        renderer.setGridColor(getResources().getColor(R.color.Separator_Color));
-        renderer.setBackgroundColor(getResources().getColor(R.color.abs__background_holo_light));
-        renderer.setMarginsColor(getResources().getColor(R.color.abs__background_holo_light));
-        renderer.setAxisTitleTextSize(getResources().getDimension(R.dimen.abs__action_bar_subtitle_text_size));
-        renderer.setInScroll(false);
-
-        renderer.addSeriesRenderer(0, paymentRenderer);
-        dataset.addSeries(0, new PaymentSeries(getString(R.string.paymentTotal), loan.getPayments()));
-
-        renderer.addSeriesRenderer(1, principalRenderer);
-        dataset.addSeries(1, new PrincipalSeries(getString(R.string.paymentPrincipal), loan.getPayments()));
-
-        renderer.addSeriesRenderer(2, interestRenderer);
-        dataset.addSeries(2, new InterestSeries(getString(R.string.paymentInterest), loan.getPayments()));
-
-        if (loan.hasAnyCommission()) {
-            renderer.addSeriesRenderer(3, commissionRenderer);
-            dataset.addSeries(3, new CommissionSeries(getString(R.string.paymentCommission), loan.getPayments()));
-        }
-
-        lineChart = new LineChart(dataset,renderer);
-        GraphicalView lineChartView = new GraphicalView(view.getContext(), lineChart);
-
-        LinearLayout linePlaceholder = (LinearLayout) view.findViewById(R.id.lineChartPlaceholder);
-        linePlaceholder.addView(lineChartView);
+  private class PrincipalSeries extends RangeCategorySeries {
+    public PrincipalSeries(String title, List<Payment> payments) {
+      super(title);
+      for (Payment payment : payments) {
+        double shift = 0;
+        add( shift , shift + payment.getPrincipal().doubleValue() );
+      }
     }
+  }
 
-    private void addPieChart(View view, Loan loan) {
-        DefaultRenderer renderer = new DefaultRenderer();
-        renderer.setAntialiasing(true);
-        renderer.setExternalZoomEnabled(false);
-        renderer.setPanEnabled(false);
-        renderer.setZoomEnabled(false);
-
-        renderer.setLabelsColor(getResources().getColor(R.color.abs__bright_foreground_holo_light));
-        renderer.setLabelsTextSize(getResources().getDimension(R.dimen.abs__action_bar_subtitle_text_size));
-        renderer.setBackgroundColor(getResources().getColor(R.color.abs__background_holo_light));
-        renderer.setMargins(new int[]{10,10,10,10});
-        CategorySeries series = new CategorySeries("Pie");
-
-        series.add(getString(R.string.paymentPrincipal), loan.getAmount().doubleValue());
-        SimpleSeriesRenderer principalRenderer = new SimpleSeriesRenderer();
-        principalRenderer.setDisplayChartValues(true);
-        principalRenderer.setColor(getResources().getColor(R.color.principal));
-        renderer.addSeriesRenderer(0, principalRenderer);
-
-
-
-        series.add(getString(R.string.paymentInterest), loan.getTotalInterests().doubleValue());
-        SimpleSeriesRenderer interestRenderer = new SimpleSeriesRenderer();
-        interestRenderer.setDisplayChartValues(true);
-        interestRenderer.setColor(getResources().getColor(R.color.interest));
-        renderer.addSeriesRenderer(1, interestRenderer);
-
-
-        if (loan.hasAnyCommission()) {
-            series.add(getString(R.string.paymentCommission), loan.getCommissionsTotal().doubleValue());
-            SimpleSeriesRenderer commissionRenderer = new SimpleSeriesRenderer();
-            commissionRenderer.setDisplayChartValues(true);
-            commissionRenderer.setColor(getResources().getColor(R.color.commission));
-            renderer.addSeriesRenderer(2, commissionRenderer);
-        }
-
-        pieChart = new PieChart(series , renderer){
-            @Override
-            public void draw(Canvas canvas, int x, int y, int width, int height, Paint paint) {
-                setCenterX( width/2);
-                setCenterY( height/2);
-                super.draw(canvas, x, y, width, height, paint);
-            }
-        };
-        GraphicalView pieChartView = new GraphicalView(view.getContext(), pieChart);
-        LinearLayout placeholder = (LinearLayout) view.findViewById(R.id.pieChartPlaceholder);
-        placeholder.addView(pieChartView);
+  private class InterestSeries extends RangeCategorySeries {
+    public InterestSeries(String title, List<Payment> payments) {
+      super(title);
+      for (Payment payment : payments) {
+        double shift = payment.getPrincipal().doubleValue();
+        add( shift , shift + payment.getInterest().doubleValue() );
+      }
     }
+  }
 
-
-    private class PrincipalSeries extends XYSeries {
-        public PrincipalSeries(String title, List<Payment> payments) {
-            super(title);
-            for (Payment payment : payments) {
-                add(payment.getNr().doubleValue(), payment.getPrincipal().doubleValue());
-            }
-        }
+  private class CommissionSeries extends RangeCategorySeries {
+    public CommissionSeries(String title, List<Payment> payments) {
+      super(title);
+      for (Payment payment : payments) {
+        double shift = payment.getPrincipal().doubleValue() + payment.getInterest().doubleValue();
+        add( shift , shift + payment.getCommission().doubleValue() );
+      }
     }
+  }
 
-    private class InterestSeries extends XYSeries {
-        public InterestSeries(String title, List<Payment> payments) {
-            super(title);
-            for (Payment payment : payments) {
-                add(payment.getNr().doubleValue(), payment.getInterest().doubleValue());
-            }
-        }
-    }
 
-    private class CommissionSeries extends XYSeries {
-        public CommissionSeries(String title, List<Payment> payments) {
-            super(title);
-            for (Payment payment : payments) {
-                add(payment.getNr().doubleValue(), payment.getCommission().doubleValue());
-            }
-        }
-    }
-
-    private class PaymentSeries extends XYSeries {
-        public PaymentSeries(String title, List<Payment> payments) {
-            super(title);
-            for (Payment payment : payments) {
-                add(payment.getNr().doubleValue(), payment.getAmount().doubleValue());
-            }
-        }
-    }
 }
--- branches/next/src/ee/smkv/calc/loan/ScheduleFragment.java
+++ branches/next/src/ee/smkv/calc/loan/ScheduleFragment.java
@@ -1,13 +1,13 @@
 package ee.smkv.calc.loan;
 
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockFragment;
@@ -19,125 +19,126 @@ import java.util.List;
 
 public class ScheduleFragment extends SherlockFragment {
 
-    private CreateScheduleTableTask scheduleTableTask;
+  private CreateScheduleTableTask scheduleTableTask;
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.schedule, container, false);
+
+    Loan loan = StartActivity.loan;
+
+    scheduleTableTask = new CreateScheduleTableTask(view.getContext() , (ProgressBar)view.findViewById(R.id.scheduleLoading));
+
+    List<Payment> payments = loan.getPayments();
+    Payment[] array = new Payment[payments.size()];
+    scheduleTableTask.execute(payments.toArray(array));
+
+    return view;
+  }
+
+  @Override
+  public void onDetach() {
+    if (scheduleTableTask != null) {
+      scheduleTableTask.cancel(true);
+    }
+    super.onDetach();
+  }
+
+  public class CreateScheduleTableTask extends AsyncTask<Payment, Integer, List<View>> {
+
+    private final Context context;
+    private final ProgressBar progressBar;
+
+
+    public CreateScheduleTableTask(Context context , ProgressBar progressBar) {
+      this.context = context;
+      this.progressBar = progressBar;
+
+    }
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.schedule, container, false);
-
-        Loan loan = StartActivity.loan;
-
-        scheduleTableTask = new CreateScheduleTableTask(view.getContext());
-
-        List<Payment> payments = loan.getPayments();
-        Payment[] array = new Payment[payments.size()];
-        scheduleTableTask.execute(payments.toArray(array));
-
-        return view;
+    protected void onPreExecute() {
+      progressBar.setVisibility(View.VISIBLE);
     }
 
     @Override
-    public void onDetach() {
-        if (scheduleTableTask != null) {
-            scheduleTableTask.cancel(true);
+    protected List<View> doInBackground(Payment... payments) {
+      this.progressBar.setMax(payments.length);
+      List<View> list = new ArrayList<View>(payments.length + 1);
+
+      Loan loan = StartActivity.loan;
+      boolean hasAnyCommission = loan.hasAnyCommission();
+
+
+      if (loan.hasDownPayment() || loan.hasDisposableCommission()) {
+        View tableRow = LayoutInflater.from(context).inflate(R.layout.schedule_row, null);
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentNr), 0);
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentBalance), loan.getAmount());
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentPrincipal), loan.getDownPaymentPayment());
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentInterest), 0);
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentCommission), loan.getDisposableCommissionPayment());
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentAmount), loan.getDownPaymentPayment());
+        if (!hasAnyCommission) {
+          tableRow.findViewById(R.id.schedulePaymentCommission).setVisibility(View.GONE);
         }
-        super.onDetach();
+        list.add(tableRow);
+        this.publishProgress(0);
+      }
+
+      for (Payment payment : payments) {
+        View tableRow = LayoutInflater.from(context).inflate(R.layout.schedule_row, null);
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentNr), payment.getNr());
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentBalance), payment.getBalance());
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentPrincipal), payment.getPrincipal());
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentInterest), payment.getInterest());
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentCommission), payment.getCommission());
+        Utils.setNumber((TextView)tableRow.findViewById(R.id.schedulePaymentAmount), payment.getAmount());
+        if (!hasAnyCommission) {
+          tableRow.findViewById(R.id.schedulePaymentCommission).setVisibility(View.GONE);
+        }
+        list.add(tableRow);
+        this.publishProgress(payment.getNr());
+      }
+
+      return list;
     }
 
-    public class CreateScheduleTableTask extends AsyncTask<Payment, Integer, List<View>> {
-
-        private final Context context;
-        private final ProgressDialog dialog;
-
-        public CreateScheduleTableTask(Context context) {
-            this.context = context;
-            dialog = new ProgressDialog(this.context);
-        }
-
-
-        @Override
-        protected void onPreExecute() {
-            this.dialog.setIndeterminate(false);
-            this.dialog.setMessage("Creating payments table...");
-            this.dialog.show();
-        }
-
-        @Override
-        protected List<View> doInBackground(Payment... payments) {
-            this.dialog.setMax(payments.length);
-            List<View> list = new ArrayList<View>(payments.length + 1);
-
-            Loan loan = StartActivity.loan;
-            boolean hasAnyCommission = loan.hasAnyCommission();
-
-
-            if (loan.hasDownPayment() || loan.hasDisposableCommission()) {
-                View tableRow = LayoutInflater.from(context).inflate(R.layout.schedule_row, null);
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentNr), 0);
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentBalance), loan.getAmount());
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentPrincipal), loan.getDownPaymentPayment());
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentInterest), 0);
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentCommission), loan.getDisposableCommissionPayment());
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentAmount), loan.getDownPaymentPayment());
-                if (!hasAnyCommission) {
-                    tableRow.findViewById(R.id.schedulePaymentCommission).setVisibility(View.GONE);
-                }
-                list.add(tableRow);
-                this.publishProgress(0);
-            }
-
-            for (Payment payment : payments) {
-                View tableRow = LayoutInflater.from(context).inflate(R.layout.schedule_row, null);
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentNr), payment.getNr());
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentBalance), payment.getBalance());
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentPrincipal), payment.getPrincipal());
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentInterest), payment.getInterest());
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentCommission), payment.getCommission());
-                Utils.setNumber((TextView) tableRow.findViewById(R.id.schedulePaymentAmount), payment.getAmount());
-                if (!hasAnyCommission) {
-                    tableRow.findViewById(R.id.schedulePaymentCommission).setVisibility(View.GONE);
-                }
-                list.add(tableRow);
-                this.publishProgress(payment.getNr());
-            }
-
-            return list;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-            this.dialog.setProgress(values[0]);
-        }
-
-        @Override
-        protected void onPostExecute(List<View> views) {
-            TableLayout table = (TableLayout) getView().findViewById(R.id.scheduleTable);
-            if (!StartActivity.loan.hasAnyCommission()) {
-                getView().findViewById(R.id.paymentCommissionHeader).setVisibility(View.GONE);
-            }
-            for (View view : views) {
-                table.addView(view);
-            }
-            closeDialog();
-            scheduleTableTask = null;
-        }
-
-        private void closeDialog() {
-            try {
-                if (this.dialog.isShowing()) {
-                    this.dialog.dismiss();
-                }
-            } catch (Exception ignore) {
-
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            super.onCancelled();
-            closeDialog();
-        }
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+      super.onProgressUpdate(values);
+      this.progressBar.setProgress(values[0]);
     }
+
+    @Override
+    protected void onPostExecute(List<View> views) {
+      TableLayout table = (TableLayout)getView().findViewById(R.id.scheduleTable);
+      if (!StartActivity.loan.hasAnyCommission()) {
+        getView().findViewById(R.id.paymentCommissionHeader).setVisibility(View.GONE);
+      }
+      for (View view : views) {
+        table.addView(view);
+      }
+      getView().findViewById(R.id.scheduleScrollView).setVisibility(View.VISIBLE);
+      closeDialog();
+      scheduleTableTask = null;
+    }
+
+    private void closeDialog() {
+      try {
+        progressBar.setVisibility(View.GONE);
+      }
+      catch (Exception ignore) {
+
+      }
+    }
+
+    @Override
+    protected void onCancelled() {
+      super.onCancelled();
+      closeDialog();
+    }
+  }
 
 }
